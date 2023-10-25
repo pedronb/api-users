a8e63903aa5abe7c21fd51b46476cc76138a68b3
Pedro Bezerra <pedro.bezerra@inovahu.com>
2023-10-25 17:30:35 -0300

    feat:  ensure MongoGetUsersRepository gets mongo

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,15 +1,19 @@
 import { IGetUsersRepository } from "../../controllers/get-users/protocols";
+import { MongoClient } from "../../database/mongo";
 import { User } from "../../models/user";
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
   async getUsers(): Promise<User[]> {
-    return [
-      {
-        firstName: "Pedro",
-        lastName: "Bezerra",
-        email: "pedro@bezerra.com",
-        password: "12345",
-      },
-    ];
+    const users = await MongoClient.db
+      .collection<Omit<User, "id">>("users")
+      .find({})
+      .toArray(); // o Omit vai omitir uma propriedade especifica da interface passada, no caso o id;
+
+    // porque o mongo client ja vai me retornar um id padrão
+
+    return users.map(({ _id, ...rest }) => ({
+      ...rest,
+      id: _id.toHexString(),
+    }));
   }
 }
