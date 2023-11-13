ba0cb23ddc8901a29036519ecdd2f27fd2d7dad5
Pedro Bezerra <pedro.bezerra@inovahu.com>
2023-11-13 20:37:22 -0300

    feat: add deleteUserController and routes

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,8 @@ import { MongoCreateUserRepository } from "./repositories/create-users/mongo-cre
 import { CreateUserController } from "./controllers/create-users/create-users";
 import { MongoUpdateUserRepository } from "./repositories/update-users/mongo-update-user";
 import { UpdateUserController } from "./controllers/update-users/update-user";
+import { MongoDeleteUserRepository } from "./repositories/delete-users/mongo-delete-user";
+import { DeleteUserController } from "./controllers/delete-users/delete-user";
 
 const main = async () => {
   config();
@@ -50,6 +52,20 @@ const main = async () => {
 
     const { body, statusCode } = await updateUserController.handle({
       body: req.body,
+      params: req.params,
+    });
+
+    res.status(statusCode).send(body);
+  });
+
+  app.delete("/users/:id", async (req, res) => {
+    const mongoDeleteUserRepository = new MongoDeleteUserRepository();
+
+    const deleteUserController = new DeleteUserController(
+      mongoDeleteUserRepository
+    );
+
+    const { body, statusCode } = await deleteUserController.handle({
       params: req.params,
     });
 
