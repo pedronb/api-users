dacd94a358b53300b96e6a90d2a43344923eb952
Pedro Bezerra <pedro.bezerra@inovahu.com>
2023-11-13 20:06:25 -0300

    feat: create updateUserController

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import { GetUsersController } from "./controllers/get-users/get-users";
 import { MongoClient } from "./database/mongo";
 import { MongoCreateUserRepository } from "./repositories/create-users/mongo-create-users";
 import { CreateUserController } from "./controllers/create-users/create-users";
+import { MongoUpdateUserRepository } from "./repositories/update-users/mongo-update-user";
+import { UpdateUserController } from "./controllers/update-users/update-user";
 
 const main = async () => {
   config();
@@ -34,6 +36,21 @@ const main = async () => {
 
     const { body, statusCode } = await createUserController.handle({
       body: req.body,
+    });
+
+    res.status(statusCode).send(body);
+  });
+
+  app.patch("/users/:id", async (req, res) => {
+    const mongoUpdateUserRepository = new MongoUpdateUserRepository();
+
+    const updateUserController = new UpdateUserController(
+      mongoUpdateUserRepository
+    );
+
+    const { body, statusCode } = await updateUserController.handle({
+      body: req.body,
+      params: req.params,
     });
 
     res.status(statusCode).send(body);
