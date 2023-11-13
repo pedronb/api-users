1f020be6f4ae19a2ed6f97dfd8acd160626dc278
Pedro Bezerra <pedro.bezerra@inovahu.com>
2023-11-13 20:46:20 -0300

    refactor: add IController common interface

--- src/controllers/create-users/protocols.ts
+++ src/controllers/create-users/protocols.ts
@@ -1,12 +1,4 @@
 import { User } from "../../models/user";
-import { HttpRequest, HttpResponse } from "../protocols";
-
-export interface ICreateUserController {
-  handle(
-    HttpRequest: HttpRequest<CreateUserParams>
-  ): Promise<HttpResponse<User>>;
-}
-
 export interface CreateUserParams {
   firstName: string;
   lastName: string;
